e159419112cddc0d47cafca9c8a0b8b1b2a2f9b9
Alok Kumar Jha <akj2947@gmail.com>
2022-08-23 23:39:06 +0530

    Create 1.c

---
+++ Practice/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct Node{
+    int data;
+    struct Node* next;
+};
+void createList(){
+    int k,n;
+    struct Node *p,*head;
+    printf("Number of nodes:");
+    scanf("%d",&n);
+    for(int k=0;k<n;k++){
+        
+    }
+}
